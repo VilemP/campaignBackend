c0df36634c48d5bdfcdd31f2f08d07e905710f8c
vilemP <vilemp@centrum.cz>
2024-12-04 09:52:20 +0100

    clean up and config fixes

--- apps/campaign-backend/vite.config.ts
+++ apps/campaign-backend/vite.config.ts
@@ -1,16 +1,11 @@
 /// <reference types="vitest" />
 import { defineConfig } from 'vite';
-import { getViteConfig } from '@nx/vite/plugins/vite-config';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
   plugins: [tsconfigPaths()],
-  ...getViteConfig(),
   test: {
     globals: true,
-    cache: {
-      dir: '../../node_modules/.vitest'
-    },
     environment: 'node',
     include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     reporters: ['default'],

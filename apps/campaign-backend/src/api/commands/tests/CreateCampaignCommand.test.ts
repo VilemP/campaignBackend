72f0e6d9ac7b6446167c134b8ffb0663e0ccb1ff
Michal Matula <michalbbmatula@gmail.com>
2024-12-04 19:16:18 +1300

    chore: remove separate test directories in favor of co-located tests

--- apps/campaign-backend/src/api/commands/tests/CreateCampaignCommand.test.ts
+++
@@ -1,7 +0,0 @@
-import { CreateCampaignCommand } from '../CreateCampaignCommand';
-
-describe('CreateCampaignCommand', () => {
-    it('should validate input payload', () => {
-        // Tests will be implemented
-    });
-});
--- apps/campaign-backend/src/api/http/tests/server.test.ts
+++
@@ -1,7 +0,0 @@
-import { createServer } from '../server';
-
-describe('HTTP Server', () => {
-    it('should handle campaign creation request', () => {
-        // Tests will be implemented
-    });
-});

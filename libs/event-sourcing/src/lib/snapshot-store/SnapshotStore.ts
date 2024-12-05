841a7e577a7e54ff203b4a902b543fc9660d6407
vilemP <vilemp@centrum.cz>
2024-12-05 10:20:00 +0100

    clean up old structures and implementations

--- libs/event-sourcing/src/lib/snapshot-store/SnapshotStore.ts
+++
@@ -1,9 +0,0 @@
-export interface Snapshot<T> {
-    state: T;
-    version: number;
-}
-
-export interface SnapshotStore {
-    saveSnapshot<T>(streamId: string, state: T, version: number): Promise<void>;
-    getLatestSnapshot<T>(streamId: string): Promise<Snapshot<T> | null>;
-}

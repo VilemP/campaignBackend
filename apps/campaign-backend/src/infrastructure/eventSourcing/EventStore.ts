c0eb722d07ce65c3883bed7ed520df5c78644f7a
vilemP <vilemp@centrum.cz>
2024-12-05 08:40:38 +0100

    remove obsolet/dupplicate stuff

--- apps/campaign-backend/src/infrastructure/eventSourcing/EventStore.ts
+++
@@ -1,7 +0,0 @@
-import { DomainEvent } from '../../domain/events/DomainEvent';
-
-export interface EventStore {
-    saveEvents(streamId: string, events: DomainEvent[], expectedVersion: number): Promise<void>;
-    getEvents(streamId: string, fromVersion?: number): Promise<DomainEvent[]>;
-    getLastVersion(streamId: string): Promise<number>;
-}

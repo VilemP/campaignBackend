d7aa64458b24f75bd95be02efc61dc9de584cfef
Petr Valek <petr.valek@cz.stroeer-labs.com>
2024-12-30 14:14:58 +0100

    Campaign payload constants extracted to single place

--- apps/campaign-backend/src/campaigns/actions/create/payload.ts
+++ apps/campaign-backend/src/campaigns/actions/create/payload.ts
@@ -4,26 +4,41 @@ import { BusinessProductType } from '../../../business-products/productTypes.js'
 
 
 export class CreateCampaignPayload {
-   
-    @ApiProperty({ description: 'Campaign UUID', example: '304cedac-5af2-45a2-b8b7-0043abeee4e3'})
+    private static readonly nameMinLength: number = 3;
+    private static readonly nameMaxLength = 100;
+    private static readonly productTypeList = Object.values(BusinessProductType).join(', ');
+
+    @ApiProperty({ 
+        description: 'Campaign UUID', 
+        example: '304cedac-5af2-45a2-b8b7-0043abeee4e3'
+    })
     @IsUUID(7, { message: 'Campaign ID must be a valid UUIDv4' })
     @IsNotEmpty()
     id!: string;
      
-    @ApiProperty({ description: 'Campaign name', example: 'Name of the campaign', minLength: 3, maxLength: 100})
+    @ApiProperty({
+        description: 'Campaign name',
+        example: 'Name of the campaign',
+        minLength: CreateCampaignPayload.nameMinLength,
+        maxLength: CreateCampaignPayload.nameMaxLength
+    })
     @IsString()
     @IsNotEmpty()
-    @Length(3, 100, { message: 'Campaign name must be between 3 and 100 characters' })
+    @Length(
+        CreateCampaignPayload.nameMinLength,
+        CreateCampaignPayload.nameMaxLength,
+        { message: `Campaign name must be between ${CreateCampaignPayload.nameMinLength} and ${CreateCampaignPayload.nameMaxLength} characters` }
+    )
     name!: string;
     
     @ApiProperty({
         isArray: true,
         enum: BusinessProductType,
-        example: `${Object.values(BusinessProductType).join('|')}`,
+        example: CreateCampaignPayload.productTypeList,
         default: BusinessProductType.STANDARD
     })
     @IsEnum(BusinessProductType, { 
-        message: `Business type must be one of: ${Object.values(BusinessProductType).join(', ')}`
+        message: `Business type must be one of: ${CreateCampaignPayload.productTypeList}`
     })
     @IsNotEmpty()
     businessType: BusinessProductType = BusinessProductType.STANDARD;
